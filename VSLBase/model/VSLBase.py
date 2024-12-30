2c1490d1e5ac7cdc24c4a70f67d3c98ffe2df96e
MARCO DEL CORE s322783 <s322783@studenti.polito.it>
2024-12-30 23:57:20 +0100

    VSLBase

--- VSLBase/model/VSLBase.py
+++ VSLBase/model/VSLBase.py
@@ -13,6 +13,38 @@ from model.layers import (
     BertEmbedding,
 )
 
+def build_optimizer_and_scheduler(model, configs):
+    no_decay = [
+        "bias",
+        "layer_norm",
+        "LayerNorm",
+    ]  # no decay for parameters of layer norm and bias
+    optimizer_grouped_parameters = [
+        {
+            "params": [
+                p
+                for n, p in model.named_parameters()
+                if not any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": 0.01,
+        },
+        {
+            "params": [
+                p
+                for n, p in model.named_parameters()
+                if any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": 0.0,
+        },
+    ]
+    optimizer = AdamW(optimizer_grouped_parameters, lr=configs.init_lr)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer,
+        configs.num_train_steps * configs.warmup_proportion,
+        configs.num_train_steps,
+    )
+    return optimizer, scheduler
+
 class VSLBase(nn.Module):
     def __init__(self, configs, word_vectors):
         super(VSLBase, self).__init__()
